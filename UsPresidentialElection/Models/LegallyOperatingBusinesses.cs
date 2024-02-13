f8a7f9f251d287cc92dc2f6394543859e7787693
MichaelPico <michael.pico.sandoval@gmail.com>
2024-02-13 08:37:55 +0100

    Added an api call

---
+++ UsPresidentialElection/Models/LegallyOperatingBusinesses.cs
@@ -0,0 +1,16 @@
+﻿namespace UsPresidentialElection.Models
+{
+    public class LegallyOperatingBusinesses
+    {
+        public string license_nbr { get; set; }
+        public string license_type { get; set; }
+        public DateTime lic_expir_dd { get; set; }
+        public string license_status { get; set; }
+        public DateTime license_creation_date { get; set; }
+        public string industry { get; set; }
+        public string business_name { get; set; }
+        public string address_city { get; set; }
+        public string address_state { get; set; }
+        public string address_zip { get; set; }
+    }
+}
---
+++ UsPresidentialElection/Services/SodaServiceHelpersHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using UsPresidentialElection.Models;
+
+public class SodaService
+{
+
+    public static async Task<IEnumerable<LegallyOperatingBusinesses>> GetLegallyOperatingBusinesses()
+    {
+        using HttpClient client = new HttpClient();
+        var data = await client.GetAsync("https://data.cityofnewyork.us/resource/w7w3-xahh.json");
+        var dataAsString = await data.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<IEnumerable<LegallyOperatingBusinesses>>(dataAsString);
+    }
+}
